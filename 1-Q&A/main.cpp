c2eab5d6c701f89df0591af768504b158791af46
IgniteCdrive <34989562+IgniteCdrive@users.noreply.github.com>
2018-01-01 18:06:19 -0500

    ok
    Ok

--- 1-Q&A/main.cpp
+++ 1-Q&A/main.cpp
@@ -6,12 +6,19 @@ using namespace std;
 // Main
 int main()
 {
-  string mystr;
-  cout << "What is your name?: ";
-  getline (cin, mystr);
-  cout << "Hello " << mystr << endl;
-  cout << "Do you want to play a game? ";
-  getline (cin, mystr);
-  cout << "In Jigsaw tone of voice: Let's play a game " << endl;
+string mystr;
+string mystrr;
+cout << "What is your name?: ";
+getline (cin, mystr);
+cout << "Hello " << mystr << endl;
+if (mystr == "Gabriel")
+cout << "Gabriel";
+else if (mystrr == "Gennix")
+cout << "Gennix";
+cout << "Do you want to play a game?" << mystr << endl;
+getline (cin,mystr);
+cout << "In Jigsaw tone of voice: Let's play a game " << mystr << endl;
+getline (cin,mystrr);
+cout << "Ending Transmission" << mystrr << endl;
     return 0;
 }
