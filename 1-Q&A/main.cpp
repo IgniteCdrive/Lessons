8f6e28262e9d0feefc30e43244ca7c50c1cebc22
IgniteCdrive <34989562+IgniteCdrive@users.noreply.github.com>
2018-01-01 16:50:37 -0500

    Summary
    I did a thing and now am the Wizard Doge. Also PS Gennix is an IDOT.

--- 1-Q&A/main.cpp
+++ 1-Q&A/main.cpp
@@ -1,8 +1,17 @@
-// Includes up here
-
+// cin with strings
+#include <iostream>
+#include <string>
+using namespace std;
 
 // Main
 int main()
 {
+  string mystr;
+  cout << "What is your name?: ";
+  getline (cin, mystr);
+  cout << "Hello " << mystr << endl;
+  cout << "Do you want to play a game? ";
+  getline (cin, mystr);
+  cout << "In Jigsaw tone of voice: Let's play a game " << endl;
     return 0;
 }
